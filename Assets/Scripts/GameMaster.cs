1714c42552573a9f8d190b7b09fb76243acd7b45
James Boggs <jmsboggs@gmail.com>
2020-07-11 17:58:19 -0400

    Refactor variable names to fit C# conventions

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -14,14 +14,14 @@ public class GameMaster : MonoBehaviour {
     public bool RandomBoardGeneration;
 
     private Tilemap_Controller TilemapController;
-    private Camera_Controller mcam_controller;
-    private GameObject[] tm_heads;
-    private TuringMachineHeadController[] tm_head_controllers;
-    private TuringMachine[] turing_machines;
+    private Camera_Controller MainCamController;
+    private GameObject[] TuringMachineHeads;
+    private TuringMachineHeadController[] TuringMachineHeadControllers;
+    private TuringMachine[] TuringMachines;
 
-    private bool run_simulation;
+    private bool RunSimulation;
     private IEnumerator TuringMachineUpdateClock(float waitTime) {
-        while (run_simulation) {
+        while (RunSimulation) {
             SimulateOneStep();
             yield return new WaitForSeconds(waitTime);
         }
@@ -29,36 +29,36 @@ public class GameMaster : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
-        tm_heads = new GameObject[NumberOfTuringMachines];
-        tm_head_controllers = new TuringMachineHeadController[NumberOfTuringMachines];
-        turing_machines = new TuringMachine[NumberOfTuringMachines];
+        TuringMachineHeads = new GameObject[NumberOfTuringMachines];
+        TuringMachineHeadControllers = new TuringMachineHeadController[NumberOfTuringMachines];
+        TuringMachines = new TuringMachine[NumberOfTuringMachines];
         for (int i = 0; i < NumberOfTuringMachines; i++) {
-            tm_heads[i] = Instantiate(TuringMachineHeadPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-            tm_head_controllers[i] = tm_heads[i].GetComponent<TuringMachineHeadController>();
-            turing_machines[i] = new TuringMachine(10);
+            TuringMachineHeads[i] = Instantiate(TuringMachineHeadPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            TuringMachineHeadControllers[i] = TuringMachineHeads[i].GetComponent<TuringMachineHeadController>();
+            TuringMachines[i] = new TuringMachine(10);
         }
 
         TilemapController = GridTilemap.GetComponent<Tilemap_Controller>();
-        mcam_controller = MainCamera.GetComponent<Camera_Controller>();
-        foreach (TuringMachine tm in turing_machines) {
+        MainCamController = MainCamera.GetComponent<Camera_Controller>();
+        foreach (TuringMachine tm in TuringMachines) {
             tm.InitWithRandomTransitions();
             print(tm.ToString());
         }
 
-        run_simulation = true;
+        RunSimulation = true;
         IEnumerator simulationUpdater = TuringMachineUpdateClock(1.0f);
         StartCoroutine(simulationUpdater);
     }
 
     // Update is called once per frame
     void Update() {
-        if (mcam_controller.moved) {
+        if (MainCamController.moved) {
             if (RandomBoardGeneration) {
-                TilemapController.RandomPopulateGridAtView(mcam_controller.world_view_rect);
+                TilemapController.RandomPopulateGridAtView(MainCamController.world_view_rect);
             } else {
-                TilemapController.PopulateGridAtView(mcam_controller.world_view_rect);
+                TilemapController.PopulateGridAtView(MainCamController.world_view_rect);
             }
-            mcam_controller.moved = false;
+            MainCamController.moved = false;
         }
 
         if (Input.GetMouseButtonUp((int)MouseButton.LeftMouse)) {
@@ -76,20 +76,20 @@ public class GameMaster : MonoBehaviour {
     public void SimulateOneStep() {
         (TM_Direction, TM_Symbol)[] tm_output_list = new (TM_Direction, TM_Symbol)[NumberOfTuringMachines];
         for (int tm_id=0; tm_id < NumberOfTuringMachines; tm_id++) {
-            Vector3 tm_world_loc = tm_heads[tm_id].transform.position;
+            Vector3 tm_world_loc = TuringMachineHeads[tm_id].transform.position;
             Vector3Int tm_tile_loc = GridTilemap.WorldToCell(tm_world_loc);
             TM_Symbol tm_input_symbol = TilemapController.GetTileSymbol(tm_tile_loc);
 
-            tm_output_list[tm_id] = turing_machines[tm_id].HandleInput(tm_input_symbol);
-            print("TM " + tm_id.ToString() + " state " + turing_machines[tm_id].CurrentState.ToString() + " output " + tm_output_list[tm_id].ToString());
+            tm_output_list[tm_id] = TuringMachines[tm_id].HandleInput(tm_input_symbol);
+            print("TM " + tm_id.ToString() + " state " + TuringMachines[tm_id].CurrentState.ToString() + " output " + tm_output_list[tm_id].ToString());
         }
 
         // TODO: Resolve any conflicts
 
         for (int tm_id = 0; tm_id < NumberOfTuringMachines; tm_id++) {
-            TuringMachineHeadController tm_head_controller = tm_head_controllers[tm_id];
+            TuringMachineHeadController tm_head_controller = TuringMachineHeadControllers[tm_id];
             (TM_Direction, TM_Symbol) tm_output = tm_output_list[tm_id];
-            TuringMachine turing_machine = turing_machines[tm_id];
+            TuringMachine turing_machine = TuringMachines[tm_id];
 
             Vector3 current_head_world_loc = tm_head_controller.position;
             Vector3Int current_head_grid_loc = GridTilemap.WorldToCell(current_head_world_loc);
